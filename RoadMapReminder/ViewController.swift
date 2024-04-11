f2d1d49fc4feaf81ebe85bae21129d484e4c9cda
Ruslan <ruslan99992222@gmail.com>
2024-04-11 21:43:06 +0300

    Initial Commit

---
+++ RoadMapReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RoadMapReminder
+//
+//  Created by Руслан Абрамов on 11.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
